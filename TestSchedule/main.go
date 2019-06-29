e6f827d590d1860725444ebfd20f1cdeaf3ad164
laughingor2018 <2006cheng@163.com>
2019-06-29 06:58:29 -0700

    add TestSchedule

---
+++ TestSchedule/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"github.com/gorhill/cronexpr"
+)
+
+
+type CronJob struct {
+	expr * cronexpr.Expression
+	nextTime time.Time
+}
+
+func main(){
+	var (
+		cronJob * CronJob
+		now time.Time
+		nextTime time.Time
+		expr * cronexpr.Expression
+		cronTables map[string]*CronJob
+	)
+	
+	cronTables = make(map[string]*CronJob)
+
+	expr,_ = cronexpr.Parse("*/5 * * * * * *")
+
+	now = time.Now()
+	nextTime = expr.Next(now)
+
+	cronJob = &CronJob{
+		expr:expr,
+		nextTime:nextTime,
+	}
+
+	cronTables["job1"] = cronJob
+
+
+
+    expr,_ = cronexpr.Parse("*/5 * * * * * *")
+
+    now = time.Now()
+    nextTime = expr.Next(now)
+
+    cronJob = &CronJob{
+        expr:expr,
+        nextTime:nextTime,
+    }
+
+	cronTables["job2"] = cronJob
+
+	go func(){
+		for {
+			var (
+					cronJob * CronJob
+					now time.Time
+					jobName string
+				)
+
+				now = time.Now()
+
+				for jobName,cronJob = range cronTables {
+					if cronJob.nextTime.Before(now) || cronJob.nextTime.Equal(now){
+						go func(jobName string){
+							fmt.Println("Do ",jobName)
+						}(jobName)
+
+
+						cronJob.nextTime = cronJob.expr.Next(now)
+							fmt.Println("nextTime:",cronJob.nextTime)
+					}
+				}
+
+				time.Sleep(100 * time.Millisecond)
+		}
+
+	}()
+
+	time.Sleep(100 * time.Second)
+}
